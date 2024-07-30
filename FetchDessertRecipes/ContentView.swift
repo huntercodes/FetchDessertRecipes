5454949b2394e99c7646c6c41bf0eeb2654367ce
hunter <hdowneymac@gmail.com>
2024-07-30 16:04:25 -0500

    Initial Commit

---
+++ FetchDessertRecipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FetchDessertRecipes
+//
+//  Created by hunter downey on 7/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FetchDessertRecipes/FetchDessertRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDessertRecipesApp.swift
+//  FetchDessertRecipes
+//
+//  Created by hunter downey on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchDessertRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
